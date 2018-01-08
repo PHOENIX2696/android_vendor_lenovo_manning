52254521dcc90396b104408345622e540808d532
PHOENIX2696 <pprag2821990@gmail.com>
2018-01-08 14:57:57 +0530

    First Commit

---
+++ marino_f-vendor-blobs.mk
@@ -0,0 +1,26 @@
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/lib64,system/vendor/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/xbin,system/xbin)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/xbin,system/vendor/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/marino_f/vendor/thh,system/vendor/thh)
